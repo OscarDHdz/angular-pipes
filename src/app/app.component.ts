7de519048350d68bf52acaaf9687b2456d3461a9
Ozkar Hernández <oscardavid.hernandez.mx@gmail.com>
2018-06-17 17:59:10 -0500

    Initial commit - Some pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  nombre = 'fernando';
+  arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  PI = Math.PI;
+
+  a = 0.234;
+
+  salario = 1234.5;
+
+  heroe = {
+    nombre: 'Logan',
+    clave: 'Wolverine',
+    edad: 500,
+    direccion: {
+      calle: 'Xavier street',
+      number: 606
+    }
+  };
+
+  valorDePromesa = new Promise( (resolve, reject) => {
+    setTimeout(() => resolve('Llego la data'), 1500);
+  });
+
+  hoy = new Date();
 }
